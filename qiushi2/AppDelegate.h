67a036e5042d941fc29f1082ee6bac9389d28154
workcorp <workcorp@sina.com>
2012-10-15 16:53:15 +0800

    Initial Commit

---
+++ qiushi2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  qiushi2
+//
+//  Created by telecom on 12-10-15.
+//  Copyright (c) 2012年 workcorp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ qiushi2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  qiushi2
+//
+//  Created by telecom on 12-10-15.
+//  Copyright (c) 2012年 workcorp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
